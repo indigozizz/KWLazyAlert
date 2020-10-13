1352101bc3ae4e1118bcb95826e7eb8170460a01
Kawa Chen <kawa5292@gmail.com>
2020-10-13 13:28:13 +0800

    Pass Touches behind KWPassthroughtView

--- KWLazyPresent/KWLazyPresent/KWPassthroughView.m
+++ KWLazyPresent/KWLazyPresent/KWPassthroughView.m
@@ -17,4 +17,12 @@
 }
 */
 
+-(BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    for (UIView *view in self.subviews) {
+        if (!view.hidden && view.alpha > 0 && view.userInteractionEnabled && [view pointInside:[self convertPoint:point toView:view] withEvent:event])
+            return YES;
+    }
+    return NO;
+}
+
 @end
